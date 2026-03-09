8bf2fcea3760c6d6bdf5d732ba3f30d6a1b2dfa8
tosinayo2257-commits <tosinayo2257@gmail.com>
2026-03-09 15:12:56 +0100

    updated design and fixed bugs

--- src/Component/HomepageComponent/Features.jsx
+++ src/Component/HomepageComponent/Features.jsx
@@ -19,9 +19,9 @@ export default function Features() {
         </p>
       </div>
 
-      <div className="flex flex-col max-w-7xl mx-auto px-4 md:px-10 gap-20">
+      <div className="flex flex-col max-w-6xl mx-auto px-4 md:px-10 gap-20">
         {/* Feature 1 */}
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-12">
           {/* Image */}
           <div className="rounded-3xl bg-[#362FF6] flex justify-center mb-5 md:mb-5 pt-8 ">
             <img
@@ -37,11 +37,12 @@ export default function Features() {
               Fair & Efficient Dispute Resolution
             </h2>
 
-            <p className="text-[#000000B2] max-w-xl md:ml-auto mx-auto">
+            <p className="text-[#000000B2] max-w-126 md:ml-auto mx-auto">
               Disagreements can happen. Our streamlined dispute resolution
               process, powered by an initial AI review and backed by expert
               human support, ensures that issues are addressed quickly,
-              transparently, and impartially.
+              transparently, and impartially. We're committed to finding fair
+              solutions so you can move forward.
             </p>
 
             <button className="mt-8 bg-[#362FF6] hover:bg-black text-white font-medium px-6 py-3 rounded-xl transition">
@@ -51,7 +52,7 @@ export default function Features() {
         </div>
 
         {/* Feature 2 */}
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-12">
           {/* Image */}
           <div className="rounded-3xl bg-[#362FF6] flex justify-center mb-5 md:mb-5 md:order-2">
             <img
@@ -67,10 +68,11 @@ export default function Features() {
               Secure Digital Agreement Signatures
             </h2>
 
-            <p className="text-[#000000B2] max-w-xl mx-auto md:mx-0">
+            <p className="text-[#000000B2] max-w-122 mx-auto md:mx-0">
               Formalize your commitments with ease. Create, review, and
               digitally sign agreements directly on the TrustPay platform. Both
-              parties receive a secure, verifiable copy.
+              parties receive a secure, verifiable copy, ensuring clarity and
+              mutual understanding from the outset.
             </p>
 
             <button className="mt-8 bg-[#362FF6] hover:bg-black text-white font-medium px-6 py-3 rounded-xl transition">
